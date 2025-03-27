889d6e171d2f8c7ab2c6a5831a48be98333a0f1c
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2025-03-27 10:01:15 -0600

    completed unit 6

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DepartmentsService } from '../../services/departments.service';
+import { Department } from '../../interfaces/department';
 
 @Component({
   selector: 'app-timesheet',
@@ -7,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './timesheet.component.scss'
 })
 export class TimesheetComponent {
+  departments: Department[];
+  department: Department;
 
+  constructor(
+    private route: ActivatedRoute,
+    private departmentsService: DepartmentsService,
+  ) { }
+
+  ngOnInit(): void {
+    this.departments = this.departmentsService.departments;
+    this.department = this.departments.find(department => department.id ===this.route.snapshot.params['id']);
+  }
 }
