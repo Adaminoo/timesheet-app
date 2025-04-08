3ea36803b630b26b7c62b5bda788a42c88cddf65
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2025-04-08 08:16:44 -0600

    completed unit 8

--- src/app/components/timesheet/timesheet.component.ts
+++ src/app/components/timesheet/timesheet.component.ts
@@ -17,6 +17,7 @@ export class TimesheetComponent {
   employeeNameFC = new FormControl('', this.nameValidator());
   employees: Employee[] = [];
   employeeId = 0;
+  weekdays: string[] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
   constructor(
     private route: ActivatedRoute,
@@ -37,6 +38,13 @@ export class TimesheetComponent {
             departmentId: this.department?.id,
             name: this.employeeNameFC.value,
             payRate: Math.floor(Math.random() * 50) + 50,
+            monday: 0,
+            tuesday: 0,
+            wednesday: 0,
+            thursday: 0,
+            friday: 0,
+            saturday: 0,
+            sunday: 0
         });
 
         this.employeeNameFC.setValue('');
@@ -55,5 +63,15 @@ export class TimesheetComponent {
         return error;
     };
   }
+
+  getTotalHours(employee: Employee): number {
+    return employee.monday + employee.tuesday + employee.wednesday
+        + employee.thursday + employee.friday + employee.saturday + employee.sunday;
+  }
+
+  deleteEmployee(index: number): void {
+    this.employees.splice(index, 1);
+  }
+
 }
 
